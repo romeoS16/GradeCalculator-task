0cad4a13c290e9a019d29d26c98487d79b232795
romeoS16 <romeoschweizer29@gmail.com>
2025-11-24 10:45:08 +0100

    Kommentare

--- src/test/java/GradeCalculatorTest.java
+++ src/test/java/GradeCalculatorTest.java
@@ -5,6 +5,10 @@ import static org.junit.Assert.assertEquals;
 
 public class GradeCalculatorTest {
 
+    // In dieser Gruppe nutzen Sie das test-after-Prinzip:
+    // 1. Implementieren Sie zuerst GradeCalculator.grade(int punkte).
+    // 2. Schreiben Sie danach eigene Tests für typische und Randfälle.
+
     @Test
     public void ninetyFivePoints_isGradeOne() {
         String result = GradeCalculator.grade(95);

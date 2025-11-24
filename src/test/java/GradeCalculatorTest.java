78058633d955f80ca43a0c65a678afd38256c058
romeoS16 <romeoschweizer29@gmail.com>
2025-11-24 10:45:55 +0100

    Kommentare

--- src/test/java/GradeCalculatorTest.java
+++ src/test/java/GradeCalculatorTest.java
@@ -5,6 +5,10 @@ import static org.junit.Assert.assertEquals;
 
 public class GradeCalculatorTest {
 
+    // In dieser Gruppe arbeiten Sie mit Test-Driven Development (TDD).
+    // Schreiben Sie Tests zuerst, bevor Sie die Methode implementieren
+    // oder ändern (Red – Green – Refactor).
+
     @Test
     public void ninetyFivePoints_isGradeOne() {
         String result = GradeCalculator.grade(95);
